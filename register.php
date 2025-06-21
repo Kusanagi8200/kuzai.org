483fa4f40668684bbdf09d6270a15ac8608103e5
vkz@8200e/k-S9CD <116643083+Kusanagi8200@users.noreply.github.com>
2025-06-21 23:44:49 +0200

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+require 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    
+    $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->execute([$username, $password]);
+    
+    echo "Inscription réussie. <a href='index.php'>Se connecter</a>";
+}
+?>
+
+<form method="post">
+    <input type="text" name="username" placeholder="Nom d'utilisateur" required><br>
+    <input type="password" name="password" placeholder="Mot de passe" required><br>
+    <input type="submit" value="S'inscrire">
+</form>
